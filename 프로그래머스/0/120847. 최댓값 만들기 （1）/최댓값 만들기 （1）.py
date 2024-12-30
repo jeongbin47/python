daf3055193b37566ebc695011045830751bd771d
황정빈 <102705562+jeongbin47@users.noreply.github.com>
2024-12-30 15:20:42 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    n = max(numbers)
+    n2 = numbers[0]
+    if n == n2:
+        n2 = numbers[1]
+    for i in numbers:
+        if n2 < i < n:
+            n2 = i
+    return max(numbers) * n2
